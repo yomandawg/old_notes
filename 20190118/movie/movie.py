4712bff963c29a3e111abab100f0f437d8a39928
yomandawg <yomandawgshit@gmail.com>
2019-01-19 16:42:08 +0900

    home0119_1

--- 20190118/movie/movie.py
+++ 20190118/movie/movie.py
@@ -4,72 +4,77 @@ import urllib.request
 import os
 from datetime import date, timedelta
 
-KEY_KOBIS = os.getenv("KEY_KOBIS")
-KEY_NAVER = os.getenv("KEY_NAVER")
-PW_NAVER = os.getenv("PW_NAVER")
-movieCd = []
-movieNm = []
-thumb_url = []
+# KEY_KOBIS = os.getenv("KEY_KOBIS")
+# KEY_NAVER = os.getenv("KEY_NAVER")
+# PW_NAVER = os.getenv("PW_NAVER")
 
-# week
-# 몇주치를 출력할지 선택 입력합니다.
-# weekGb
-# 주간/주말/주중을 선택 입력합니다.
-# 0 : 주간 (월~일) / 1 : 주말 (금~일) / 2 : 주중 (월~목)
+KEY_KOBIS = "08305de6d4fe4be99dd4e3293b30dfa2"
+KEY_NAVER = "xL3zdvWuqpSs08aJTPbk"
+PW_NAVER = "CBUZR1B2cH"
+
+
 def boxoffice(weeks=1, weekGb=0, key=KEY_KOBIS):
     base_url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key={}&targetDt={}&weeekGb={}'
-    box = csv.writer(open('boxoffice.csv', 'w', encoding='utf-8', newline=''))
-    box.writerow(['영화코드', '영화명', '누적관객수'])
+    boxoffice = csv.writer(open('boxoffice.csv', 'w', encoding='utf-8', newline=''))
+    # boxoffice.writerow(['showRange, 'movieCd', 'movieNm', 'audiAcc'])
 
     for week in range(weeks):
         targetDt = (date(2019, 1, 13) - timedelta(7*week)).strftime("%Y%m%d")
         url = base_url.format(key, targetDt, weekGb)
         response = requests.get(url).json().get("boxOfficeResult")
-        weekdays = response.get("showRange")
-        movieCd.append(response.get("weeklyBoxOfficeList")[0].get("movieCd"))
+        weekdays = [response.get("showRange")]
 
-        box.writerow([weekdays])
         for i in range(10):
-            result = [response.get("weeklyBoxOfficeList")[i].get(value) for value in ["movieCd", "movieNm", "audiAcc"]]
-            box.writerow(result)
-        box.writerow([])
+            result = weekdays + [response.get("weeklyBoxOfficeList")[i].get(value) for value in ["movieCd", "movieNm", "audiAcc"]]
+            boxoffice.writerow(result)
     
-def movie(movieCd=movieCd, key=KEY_KOBIS):
+    return
+
+
+def movie(key=KEY_KOBIS):
     base_url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key={}&movieCd={}'
-    box = csv.writer(open('movie.csv', 'w', encoding='utf-8', newline=''))
-    box.writerow(['영화코드', '영화명(국문)', '영화명(영문)', '영화명(원문)', '개봉연도', '상영시간', '장르', '감독명', '배우1', '배우2', '배우3', '관람등급'])
+    boxoffice = csv.reader(open("boxoffice.csv", 'r', encoding="utf-8"))
+    movie_info = csv.writer(open('movie.csv', 'w', encoding="utf-8", newline=''))
+    # movie_info.writerow(['영화코드', '영화명(국문)', '영화명(영문)', '영화명(원문)', '개봉연도', '상영시간', '장르', '감독명', '배우1', '배우2', '배우3', '관람등급'])
 
-    for code in movieCd:
-        url = base_url.format(key, code)
+    for row in boxoffice:
+        movieCd = row[1]
+        url = base_url.format(key, movieCd)
         response = requests.get(url).json().get("movieInfoResult").get("movieInfo")
-        movieNm.append(response.get("movieNm"))
-
-        result = [response.get(value) for value in ["movieCd", "movieNm", "movieNmEn", "movieNmOg", 'prdtYear', "showTm"]]
+        
+        result = [response.get(value) for value in ["movieCd", "movieNm", "movieNmEn", "movieNmOg", "prdtYear", "showTm"]]
         genre = response.get("genres")[0].get("genreNm")
         director = response.get("directors")[0].get("peopleNm")
-        actor = [ _.get("peopleNm") for _ in response.get("actors")[:3] ]
-        grade =  response.get("audits")[0].get("watchGradeNm")
+        actor = [i.get("peopleNm") for i in response.get("actors")[:3]]
+        grade = response.get("audits")[0].get("watchGradeNm")
         result += [genre, director, *actor, grade]
-        box.writerow(result)
+        movie_info.writerow(result)
+    
+    return
 
-def movie_naver(movieNm=movieNm, key=KEY_NAVER):
+def movie_naver(key=KEY_NAVER):
     base_url = 'https://openapi.naver.com/v1/search/movie.json?query={}&display=1'
     headers = {'X-Naver-Client-Id': KEY_NAVER, 'X-Naver-Client-Secret': PW_NAVER }
-    box = csv.writer(open('movie_naver.csv', 'w', encoding='utf-8', newline=''))
-    box.writerow(['movie_name', 'movie_code', 'thumb_url', 'link_url', 'user_rating'])
+    movie_info = csv.reader(open("movie.csv", "r", encoding="utf-8"))
+    movie_naver = csv.writer(open("movie_naver.csv", "w", encoding="utf-8", newline=""))
+    # movie_naver.writerow(['movie_name', 'movie_code', 'thumb_url', 'link_url', 'user_rating'])
 
-    for name, code in zip(movieNm, movieCd):
-        url = base_url.format(name)
+    for row in movie_info:
+        movieCd, movieNm = row[0], row[1]
+        url = base_url.format(movieNm)
         response = requests.get(url, headers=headers).json().get("items")[0]
-        result = [name, code] + [response.get(value) for value in ["image", "link", "userRating"]]
-        thumb_url.append(result[2])
-        box.writerow(result)
-    
-def image(thumb_url=thumb_url):
-    for thumb, code in zip(thumb_url, movieCd):
-        urllib.request.urlretrieve(thumb, f"./images/{code}.jpg")
+        result = [movieNm, movieCd] + [response.get(value) for value in ["image", "link", "userRating"]]
+        movie_naver.writerow(result)
 
-boxoffice(2)
+    return
+
+def image():
+    movie_naver = csv.reader(open("movie_naver.csv", "r", encoding="utf-8"))
+    for row in movie_naver:
+        movieCd, thumb_url = row[1], row[2]
+        urllib.request.urlretrieve(thumb_url, "./images/{}.jpg".format(movieCd))
+
+boxoffice(1)
 movie()
 movie_naver()
 image()
